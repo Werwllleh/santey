e5211cd3501300d06c4ae4f268db129f21feea00
Alexey <aaa@mworx.ru>
2024-03-13 11:46:59 +0300

    Update 2024-03-13T08:46:55.455Z

--- src/js/components/footer.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", (e) => {
-	let currentDate = new Date();
-	const currentYear = document.querySelector('.footer-bottom__left-copyright-year');
-
-	currentYear.innerHTML = `© ${currentDate.getFullYear()}`;
-});
